69e3e70afe0d66840fe46b1ff17e00c6ff438642
jsl10 <40080363+jsl10@users.noreply.github.com>
2018-07-11 16:45:06 -0400

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,16 @@
+#list in a dictionary
+"""
+Rather than putting a dictionary inside a list, it's sometimes useful to put
+a list inside a dictionary. 
+"""
+#store information about a pizza being ordered
+pizza={
+    'crust':'thick',
+    'toppings':['mushrooms','extra cheese'],
+    }
+#summarize the order
+print("You ordered a "+pizza['crust']+"-crust pizza "+
+      "with the following toppings:")
+
+for topping in pizza['toppings']:
+    print("\t"+topping)
