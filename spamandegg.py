fbf066c9521b0d77f84e05661549a4b28f53d8ea
Jae Lee <lightup2shine@gmail.com>
2018-10-03 03:29:25 -0700

    new file:   spamandegg.py

---
+++ spamandegg.py
@@ -0,0 +1,13 @@
+a = "Spam"
+b = "Egg"
+c = "Spam and Egg"
+d = input("What would you like to eat?")
+
+if d == a:
+	print ("You need Egg!")
+elif d == b:
+	print ("You need Spam!")
+elif d == c:
+	print ("Wonderful!")
+else:
+	print ("Make your mind!")
