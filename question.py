3ba92df7cf007edf5c99207d0a5cdeef882aec47
Jae Lee <lightup2shine@gmail.com>
2018-10-03 03:28:28 -0700

    new file:   question.py 	spamandegg.py

---
+++ question.py
@@ -0,0 +1,5 @@
+table = {'apple':'Crispy', 'pineapple':'Sweet', 'grape':'Oh okay' ,'pear':
+'I like it', 'lemon':'So sour!' }
+q=input("What is your favorite fruits?")
+option=table[q]
+print (option)
